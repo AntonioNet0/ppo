ecfc708fabd6b85a84c2e8eaf63defa880fe2fd1
Lucas Gabriel <lg.lajedo2@gmail.com>
2019-12-10 22:56:02 -0300

    criando home aluno

--- src/app/aluno/home-aluno/home-aluno.component.ts
+++ src/app/aluno/home-aluno/home-aluno.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Autenticacao } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home-aluno',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeAlunoComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+  private autenticaco: Autenticacao){}
   ngOnInit() {
+  }
+  public logout(): void{
+    console.log("clicou")
+    this.autenticaco.logout()
   }
 
 }
